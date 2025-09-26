a1d977502622ecd739bc4e0529a1b157fb34929b
RodrigoGutierrezPacheco <rodrigogutierrezpacheco@gmail.com>
2025-09-25 22:29:11 -0600

    modificacion

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -88,7 +88,7 @@ export default function Login() {
   };
 
   return (
-    <div className="min-h-[calc(100vh-60px)] bg-gray-100 flex items-center justify-center p-1 sm:p-4 pt-8 sm:pt-4">
+    <div className="min-h-[calc(93vh-60px)] bg-gray-100 flex items-center justify-center p-1 sm:p-4 sm:pt-4">
       <Card className="w-full max-w-md border-2 border-black relative">
         <CardContent className="p-4 sm:p-6 lg:p-8">
           <div className="text-center mb-6 sm:mb-8">
